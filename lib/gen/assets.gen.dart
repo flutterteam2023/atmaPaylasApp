7333dc68bc4fb6839cfa76d0e84c1ed3db13165c
Berke Uğur <berkeugur67@gmail.com>
2023-12-27 18:23:12 +0300

    bottom nav bar

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,276 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsBottomnavbarGen {
+  const $AssetsBottomnavbarGen();
+
+  /// File path: assets/bottomnavbar/add-circle-sharp.svg
+  String get addCircleSharp => 'assets/bottomnavbar/add-circle-sharp.svg';
+
+  /// File path: assets/bottomnavbar/document-text-outline.svg
+  String get documentTextOutline =>
+      'assets/bottomnavbar/document-text-outline.svg';
+
+  /// File path: assets/bottomnavbar/document-text.svg
+  String get documentText => 'assets/bottomnavbar/document-text.svg';
+
+  /// File path: assets/bottomnavbar/home-outline.svg
+  String get homeOutline => 'assets/bottomnavbar/home-outline.svg';
+
+  /// File path: assets/bottomnavbar/home.svg
+  String get home => 'assets/bottomnavbar/home.svg';
+
+  /// File path: assets/bottomnavbar/mail-outline.svg
+  String get mailOutline => 'assets/bottomnavbar/mail-outline.svg';
+
+  /// File path: assets/bottomnavbar/mail.svg
+  String get mail => 'assets/bottomnavbar/mail.svg';
+
+  /// File path: assets/bottomnavbar/person-outline.svg
+  String get personOutline => 'assets/bottomnavbar/person-outline.svg';
+
+  /// File path: assets/bottomnavbar/person.svg
+  String get person => 'assets/bottomnavbar/person.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        addCircleSharp,
+        documentTextOutline,
+        documentText,
+        homeOutline,
+        home,
+        mailOutline,
+        mail,
+        personOutline,
+        person
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/bicycle.png
+  AssetGenImage get bicycle => const AssetGenImage('assets/images/bicycle.png');
+
+  /// File path: assets/images/bmw.png
+  AssetGenImage get bmw => const AssetGenImage('assets/images/bmw.png');
+
+  /// File path: assets/images/clockdemo.png
+  AssetGenImage get clockdemo =>
+      const AssetGenImage('assets/images/clockdemo.png');
+
+  /// File path: assets/images/demoo.png
+  AssetGenImage get demoo => const AssetGenImage('assets/images/demoo.png');
+
+  /// File path: assets/images/line.png
+  AssetGenImage get line => const AssetGenImage('assets/images/line.png');
+
+  /// File path: assets/images/logo.png
+  AssetGenImage get logo => const AssetGenImage('assets/images/logo.png');
+
+  /// File path: assets/images/persondemo.png
+  AssetGenImage get persondemo =>
+      const AssetGenImage('assets/images/persondemo.png');
+
+  /// File path: assets/images/splashLogo.png
+  AssetGenImage get splashLogo =>
+      const AssetGenImage('assets/images/splashLogo.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values =>
+      [bicycle, bmw, clockdemo, demoo, line, logo, persondemo, splashLogo];
+}
+
+class $AssetsSvgGen {
+  const $AssetsSvgGen();
+
+  /// File path: assets/svg/Email.svg
+  String get email => 'assets/svg/Email.svg';
+
+  /// File path: assets/svg/add-circle-sharp.svg
+  String get addCircleSharp => 'assets/svg/add-circle-sharp.svg';
+
+  /// File path: assets/svg/alert-circle-outline.svg
+  String get alertCircleOutline => 'assets/svg/alert-circle-outline.svg';
+
+  /// File path: assets/svg/ban-outline.svg
+  String get banOutline => 'assets/svg/ban-outline.svg';
+
+  /// File path: assets/svg/bookmark-outline.svg
+  String get bookmarkOutline => 'assets/svg/bookmark-outline.svg';
+
+  /// File path: assets/svg/bookmark.svg
+  String get bookmark => 'assets/svg/bookmark.svg';
+
+  /// File path: assets/svg/checkmark.svg
+  String get checkmark => 'assets/svg/checkmark.svg';
+
+  /// File path: assets/svg/close-circle.svg
+  String get closeCircle => 'assets/svg/close-circle.svg';
+
+  /// File path: assets/svg/document-text-outline.svg
+  String get documentTextOutline => 'assets/svg/document-text-outline.svg';
+
+  /// File path: assets/svg/ellipsis-horizontal.svg
+  String get ellipsisHorizontal => 'assets/svg/ellipsis-horizontal.svg';
+
+  /// File path: assets/svg/gallery-edit.svg
+  String get galleryEdit => 'assets/svg/gallery-edit.svg';
+
+  /// File path: assets/svg/home.svg
+  String get home => 'assets/svg/home.svg';
+
+  /// File path: assets/svg/image-outline.svg
+  String get imageOutline => 'assets/svg/image-outline.svg';
+
+  /// File path: assets/svg/imageico.svg
+  String get imageico => 'assets/svg/imageico.svg';
+
+  /// File path: assets/svg/log-out-outline.svg
+  String get logOutOutline => 'assets/svg/log-out-outline.svg';
+
+  /// File path: assets/svg/mail-outline.svg
+  String get mailOutline => 'assets/svg/mail-outline.svg';
+
+  /// File path: assets/svg/notifications-outline.svg
+  String get notificationsOutline => 'assets/svg/notifications-outline.svg';
+
+  /// File path: assets/svg/person-outline.svg
+  String get personOutline => 'assets/svg/person-outline.svg';
+
+  /// File path: assets/svg/search-normal.svg
+  String get searchNormal => 'assets/svg/search-normal.svg';
+
+  /// File path: assets/svg/search-primary.svg
+  String get searchPrimary => 'assets/svg/search-primary.svg';
+
+  /// File path: assets/svg/send.svg
+  String get send => 'assets/svg/send.svg';
+
+  /// File path: assets/svg/share-social-outline.svg
+  String get shareSocialOutline => 'assets/svg/share-social-outline.svg';
+
+  /// File path: assets/svg/tick-circle.svg
+  String get tickCircle => 'assets/svg/tick-circle.svg';
+
+  /// File path: assets/svg/trash-outline.svg
+  String get trashOutline => 'assets/svg/trash-outline.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        email,
+        addCircleSharp,
+        alertCircleOutline,
+        banOutline,
+        bookmarkOutline,
+        bookmark,
+        checkmark,
+        closeCircle,
+        documentTextOutline,
+        ellipsisHorizontal,
+        galleryEdit,
+        home,
+        imageOutline,
+        imageico,
+        logOutOutline,
+        mailOutline,
+        notificationsOutline,
+        personOutline,
+        searchNormal,
+        searchPrimary,
+        send,
+        shareSocialOutline,
+        tickCircle,
+        trashOutline
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsBottomnavbarGen bottomnavbar = $AssetsBottomnavbarGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+  static const $AssetsSvgGen svg = $AssetsSvgGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
